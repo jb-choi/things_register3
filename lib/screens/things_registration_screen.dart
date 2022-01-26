6060aed2c0315baece4c10c594838020d3f40687
JB_choi <jjjchlwnsqja@gmail.com>
2022-01-26 15:52:08 +0900

    22_01_26

--- lib/screens/things_registration_screen.dart
+++ lib/screens/things_registration_screen.dart
@@ -4,25 +4,3 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:things_register3/widgets/camera_widget.dart';
 
 
-
-Future<void> main() async {
-  // Ensure that plugin services are initialized so that `availableCameras()`
-  // can be called before `runApp()`
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Obtain a list of the available cameras on the device.
-  final cameras = await availableCameras();
-
-  // Get a specific camera from the list of available cameras.
-  final firstCamera = cameras.first;
-
-  runApp(
-    MaterialApp(
-      theme: ThemeData.dark(),
-      home: TakePictureScreen(
-        // Pass the appropriate camera to the TakePictureScreen widget.
-        camera: firstCamera,
-      ),
-    ),
-  );
-}
